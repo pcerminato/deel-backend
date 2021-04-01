c489e15d37d6227020a6ff6f77eac4bed25f5dba
cerminatop <Pablo.Cerminato@dowjones.com>
2021-04-01 14:07:30 -0300

    includes app config files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-const app = require('./app');
+const app = require("./app");
+const config = require("config");
+const PORT = config.get("server.port");
 
 init();
 
 async function init() {
   try {
-    app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
+    app.listen(PORT, () => {
+      console.log(`Express App Listening on Port ${PORT}`);
     });
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
