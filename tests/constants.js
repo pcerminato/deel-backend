a659c7ca10e426f1b9f6e21562105c9aabceece7
cerminatop <Pablo.Cerminato@dowjones.com>
2021-04-01 14:17:42 -0300

    uses config

--- tests/constants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  BASE_URI:
-    process.env.NODE_ENV === "production" ? "" : "http://localhost:3001",
-};
--- tests/server-is-alive.test.js
+++ tests/server-is-alive.test.js
@@ -1,14 +1,15 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
-const { BASE_URI } = require("./constants");
+const { url, port } = require("config").get("server");
+const serverBaseUrl = `${url}:${port}`;
 
 chai.use(chaiHttp);
 
 describe("Health test", function () {
   it("server is alive", function (done) {
     chai
-      .request(BASE_URI)
+      .request(serverBaseUrl)
       .get("/_health")
       .end(function (err, res) {
         expect(res).to.have.status(200);
